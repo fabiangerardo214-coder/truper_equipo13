34b8a68b8fd820cf372e29c71846e88ac122dad1
Fabian Medina <fabiangerardo214@gmail.com>
2026-06-01 02:38:44 -0600

    mensaje del cambio

--- login.php
+++ login.php
@@ -30,7 +30,7 @@ if(isset($_POST['entrar'])){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Iniciar Sesion - TRUPER</title>
+    <title>Login TRUPER</title>
 
     <link rel="stylesheet"
     href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"/>
