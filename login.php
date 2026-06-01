e43cbfdc83b92fae3d13fc829b0646b27cbaa027
Fabian Medina <fabiangerardo214@gmail.com>
2026-06-01 09:24:01 -0600

    Login normal

--- login.php
+++ login.php
@@ -30,7 +30,7 @@ if(isset($_POST['entrar'])){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>INICIAR SESION</title>
+    <title>Login - Truper</title>
 
     <link rel="stylesheet"
     href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"/>
@@ -168,7 +168,7 @@ if(isset($_POST['entrar'])){
 
 <div class="login-box">
 
-    <h1>TRUPER LOGIN</h1>
+    <h1>Login - Truper</h1>
 
     <?php if(isset($error)){ ?>
         <div class="error">
